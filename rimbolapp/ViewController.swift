7a0e48cb33fc2d68f4470d74ae4159ed00c21113
최건휘 <kuun@kuun.local>
2017-03-10 00:00:20 +0900

    Initial Commit

---
+++ rimbolapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rimbolapp
+//
+//  Created by 최건휘 on 10/03/2017.
+//  Copyright © 2017 Kuun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
